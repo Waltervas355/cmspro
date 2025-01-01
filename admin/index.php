4a6f7a94246f68682fd5337e5e9e88fc6d7c93de
Waltervas355 <158066087+Waltervas355@users.noreply.github.com>
2025-01-01 14:25:07 -0600

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+require_once "controlador/ctrPlantilla.php";
+require_once "controlador/ctrRutas.php";
+require_once "controlador/ctrGrupos.php";
+require_once "controlador/ctrCategorias.php";
+require_once "controlador/ctrPerfiles.php";
+require_once "controlador/ctrAnuncios.php";
+
+require_once "modelo/modelo.grupos.php";
+require_once "modelo/modelo.categorias.php";
+require_once "modelo/modelo.perfiles.php";
+require_once "modelo/modelo.anuncios.php";
+
+
+$plantilla = plantilla::ctrPlantilla();
+
+
+?>
